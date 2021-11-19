c10e014edff78587c02f7dd613a5dc13ac685f22
e_rcabre <e_rcabre@mercadona.com>
2021-11-19 16:03:41 +0100

    primeros pasos con cli

--- pipeline-cli/src/main/groovy/com/pipeline/cli/PipelineRuntime.groovy
+++ pipeline-cli/src/main/groovy/com/pipeline/cli/PipelineRuntime.groovy
@@ -3,14 +3,17 @@ import picocli.CommandLine
 import picocli.CommandLine.Option;
 
 class PipelineRuntime implements Runnable {
+    private final GroovyClassLoader loader = new GroovyClassLoader(this.getClass().getClassLoader());
 
-    @Option(names = ["-c", "--count"], description = "number of repetitions")
-    int count = 1
+    @Option(names = ["-j", "--jenkinsfile"], description = "Jenkinsfile")
+    String jenkinsFile
 
-    void run() {
-        count.times {
-            println("hello world $it...")
-        }
+    void run() throws IllegalAccessException, InstantiationException, IOException {
+            Class pipelineClass = loader.parseClass(
+                    new File(jenkinsFile));
+            GroovyObject calc = (GroovyObject) pipelineClass.newInstance()
+            calc.invokeMethod("call", null)
+
     }
     static void main(String[] args) {
         System.exit(new CommandLine(new PipelineRuntime()).execute(args))
